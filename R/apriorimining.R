f70cac68f4a5cee81280605b01c340764a155f40
unknown <janhenrikt>
2019-09-03 14:06:26 +0200

    added placeholder for apriori

---
+++ R/apriorimining.R
@@ -0,0 +1,30 @@
+#R script containg the apriori algortihm
+#currently more of a placeholder
+
+apriorimining<-function(input, support, confidence) {
+  
+  
+  # input is changed to an object of class "purchase" called purchase_mat
+  purchase_mat <- new("purchase",
+         data = as(input,"ngCMatrix"),
+         items = colnames(input)
+  )
+  
+  colnames(purchase_mat@data) <- NULL
+  
+  # a will contain object of class "itemsets"
+  a <- freq_items(purchase = purchase_mat, supp = support)
+  
+  return(a)
+}
+
+
+######## notes
+
+library(arules)
+data("Groceries")
+bsp<-as(Groceries,"matrix")
+abc<-apriorimining(input = bsp,support = 0.01,confidence = 0.5)
+class(abc)
+show.itemsets(abc)
+
